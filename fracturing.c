4b1bd9abd3fdeb1bec9505acc17297a9e1adea74
Jordan0614-tech <jo613544@ucf.edu>
2024-09-05 01:53:47 +0000

    more fracturing

--- fracturing.c
+++ fracturing.c
@@ -51,4 +51,53 @@ printf("The perimeter of the city encompassed by your request is %.2f\n", perime
 return 2.0;
 }
 
-double calculateArea() {}
+double calculateArea() {
+    // area
+    double distance = calculateDistance();
+    double area = pow(distance, 2);
+
+    //print out
+    print("The area of the city encompassed by your request is %.2f\n", area);
+
+    // level of difficulty
+    return 5.0;
+}
+double calculateWidth() {
+    //width get inputs
+    double x1 = askForUserInput();
+    double y1 = askForUserInput();
+    double x2 = askForUserInput();
+    double y2 = askForUserInput();
+    double width = fabs(x2 - x1);
+
+    //print out
+    printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
+    printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
+    printf("The width of the city encompassed by your request is %.2f\n", width);
+
+    return 3.0;
+}
+
+//height
+double calculateHeight() {
+    double x1 = askForUserInput();
+    double y1 = askForUserInput();
+    double x2 = askForUserInput();
+    double y2 = askForUserInput();
+    double height = fabs(y2 - y1);
+
+    //print out 
+    printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
+    printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
+    printf("The height of the city encompassed by your request is %.2f\n", height);
+
+    return 4.0;
+}
+
+// attempts at bonus lol
+double askForUserInput() {
+    double value;
+    printf("Please enter a value: ");
+    scanf("%lf", &value);
+    return value;
+}
